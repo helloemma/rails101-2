f16527f6773069a6ac9160e12994810cd57ec372
emma <emmafleissig@163.com>
2017-04-02 06:31:14 +0800

    when create a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,6 +27,7 @@ params[:page], :per_page => 5)
     @group = Group.new(group_params)
     @group.user = current_user
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
